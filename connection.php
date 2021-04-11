01124f79b64bede6d6725108db6d760f4cceded0
adiferiismail <adiferiismail@gmail.com>
2021-04-11 17:23:14 +0700

    update with twig template enggine

--- connection.php
+++ connection.php
@@ -10,25 +10,3 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "SELECT * FROM product";
-$result = $conn->query($sql);
-$data =null;
-if ($result->num_rows > 0) {
-  // output data of each row
-	foreach ($result as $index => $value) {
-		$data[$index] = $value;
-		$data[$index]['detail'] = [];
-		$qq = mysqli_query($conn,"SELECT * FROM product_detail where id_product=".$value['id']);
-		foreach ($qq as $i => $value) {
-			$data[$index]['detail'][$i] = $value;
-		}
-	}
-}
-
-// echo $data;
-// $myJSON = json_encode($data,JSON_FORCE_OBJECT);
-// echo $myJSON;
-
-$conn->close();
-?>
